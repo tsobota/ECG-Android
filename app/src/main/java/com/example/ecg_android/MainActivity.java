c5dc3fab8fbabf093441a440f10a8a22cb891782
Damianonos <majorownia@gmail.com>
2019-03-29 21:47:35 +0100

    Add colors

--- app/src/main/java/com/example/ecg_android/MainActivity.java
+++ app/src/main/java/com/example/ecg_android/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.ecg_android;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -21,6 +22,10 @@ public class MainActivity extends AppCompatActivity {
                 final CharSequence text = "Connect Button";
                 int duration = Toast.LENGTH_SHORT;
                 Toast toast = Toast.makeText(context, text, duration);
+
+                connectButton.setBackgroundColor(Color.BLACK);
+                connectButton.setTextColor(Color.BLACK);
+
                 toast.show();
             }
         });
