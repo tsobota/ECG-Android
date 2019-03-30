d3a4d03fb56863c091f4ac4e0f12b658994b5a92
Damianonos <majorownia@gmail.com>
2019-03-30 02:34:11 +0100

    Static method invokings.

--- app/src/main/java/com/example/ecg_android/LayoutInitializer.java
+++ app/src/main/java/com/example/ecg_android/LayoutInitializer.java
@@ -2,7 +2,7 @@ package com.example.ecg_android;
 
 public class LayoutInitializer {
 
-    public void doInit() {
+    public static void doInit() {
         Util_Button buttonsSettings;
 //        buttonsSettings = Channel_1_ButtonController.
 //        buttonsSettings
--- app/src/main/java/com/example/ecg_android/MainActivity.java
+++ app/src/main/java/com/example/ecg_android/MainActivity.java
@@ -9,9 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
-
+        LayoutInitializer.doInit();
     }
 }
